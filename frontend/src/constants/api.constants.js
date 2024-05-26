64b31dc30fdf89579380041ad7c1aacfe23ffcc5
yashwant <yash.mangate1@gmail.com>
2024-05-26 19:28:39 +0530

    ADD: frontend base url added

--- frontend/src/constants/api.constants.js
+++ frontend/src/constants/api.constants.js
@@ -2,5 +2,6 @@ const BASE_URL = "http://localhost:3000";
 const API_v1 = `${BASE_URL}/api/v1`;
 const USER_URL = `${API_v1}/user`;
 const ACCOUNT_URL = `${API_v1}/account`;
+const CLIENT_BASE_URL = "http://localhost:5173";
 
-export { BASE_URL, USER_URL, ACCOUNT_URL };
+export { BASE_URL, USER_URL, ACCOUNT_URL, CLIENT_BASE_URL };
