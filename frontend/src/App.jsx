ad1a5bb09ae05e9096836705d5d2803a8190a0fb
yashwant <yash.mangate1@gmail.com>
2024-05-26 17:15:49 +0530

    ADD: toast context added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
-function App() {
-  return <div className="text-3xl font-bold underline">Hello world</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import { withToastProvider } from "./context/toast/ToastContext";
 
-export default App;
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default withToastProvider(App);
