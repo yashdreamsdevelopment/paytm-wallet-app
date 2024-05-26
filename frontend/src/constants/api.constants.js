7a794c6298eccff2c9456ab5887a47746f6eeced
yashwant <yash.mangate1@gmail.com>
2024-05-26 17:11:39 +0530

    ADD: constant file added

---
+++ frontend/src/constants/api.constants.js
@@ -0,0 +1,6 @@
+const BASE_URL = "http://localhost:3000";
+const API_v1 = `${BASE_URL}/api/v1`;
+const USER_URL = `${API_v1}/user`;
+const ACCOUNT_URL = `${API_v1}/account`;
+
+export { BASE_URL, USER_URL, ACCOUNT_URL };
