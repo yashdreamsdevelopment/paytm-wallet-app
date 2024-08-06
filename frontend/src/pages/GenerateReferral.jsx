fca6f8b6baca0556300214061ef719e9f72a0859
yashwant <yash.mangate1@gmail.com>
2024-08-06 14:16:12 +0530

    UPDATE: refetch functionality added

--- frontend/src/pages/GenerateReferral.jsx
+++ frontend/src/pages/GenerateReferral.jsx
@@ -26,6 +26,7 @@ const GenerateReferralPage = () => {
     isLoading: isUserReferralsLoading,
     isSuccess: isUserReferralSuccess,
     refetch,
+    isFetching,
   } = useGetUserReferralsQuery();
 
   // STATEs
@@ -70,71 +71,73 @@ const GenerateReferralPage = () => {
       console.log("## user referrals:", userReferralsData);
       setUsers(userReferralsData?.data?.referredUsers);
     }
-  }, [isUserReferralsLoading]);
+  }, [isUserReferralsLoading, isFetching]);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold mb-6">
-        Referred Users and Generate Referral Link
-      </h1>
-      <div className="flex flex-row w-full max-w-5xl bg-white shadow-md rounded-lg">
-        {/* Left Column: User List */}
-        <div className="w-1/2 p-6 border-r border-gray-200  ">
-          <h2 className="text-2xl font-semibold mb-4">
-            User List
-            <Reload cb={refetch} />
-          </h2>
-          <div className="overflow-y-auto h-96">
-            <ul className="space-y-4">
-              {users?.map((user) => (
-                <li
-                  key={user._id}
-                  className="p-4 bg-gray-100 rounded-lg shadow-sm"
+    <div className="h-full flex flex-col justify-center">
+      <div>
+        <h1 className="text-3xl font-bold mb-6">
+          Referred Users and Generate Referral Link
+        </h1>
+        <div className="flex flex-row w-full bg-white shadow-md rounded-lg">
+          {/* Left Column: User List */}
+          <div className="w-1/2 p-6 border-r border-gray-200  ">
+            <h2 className="text-2xl font-semibold mb-4">
+              User List
+              <Reload cb={refetch} />
+            </h2>
+            <div className="overflow-y-auto h-96">
+              <ul className="space-y-4">
+                {users?.map((user) => (
+                  <li
+                    key={user._id}
+                    className="p-4 bg-gray-100 rounded-lg shadow-sm"
+                  >
+                    <p className="text-lg font-medium">
+                      {user?.firstName.concat(" ", user?.lastName)}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* Right Column: Generate Referral Link */}
+          <div className="w-1/2 p-6">
+            <h2 className="text-2xl font-semibold mb-4">
+              Generate Referral Link
+            </h2>
+            <button
+              onClick={generateReferralLink}
+              className="px-4 py-2 mb-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+            >
+              Generate Referral
+            </button>
+            {showLink && (
+              <div className="flex items-center mb-4">
+                <input
+                  type="text"
+                  value={referralLink}
+                  readOnly
+                  className="px-4 py-2 w-full border border-gray-300 rounded-l-md bg-gray-100 focus:outline-none"
+                />
+                <button
+                  onClick={copyToClipboard}
+                  className="px-4 py-2 bg-green-500 text-white font-semibold rounded-r-md hover:bg-green-600"
                 >
-                  <p className="text-lg font-medium">
-                    {user?.firstName.concat(" ", user?.lastName)}
-                  </p>
-                </li>
-              ))}
-            </ul>
+                  Copy
+                </button>
+              </div>
+            )}
+            <button
+              onClick={() => navigate(-1)}
+              className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-md hover:bg-gray-600"
+            >
+              Go Back
+            </button>
           </div>
         </div>
-        {/* Right Column: Generate Referral Link */}
-        <div className="w-1/2 p-6">
-          <h2 className="text-2xl font-semibold mb-4">
-            Generate Referral Link
-          </h2>
-          <button
-            onClick={generateReferralLink}
-            className="px-4 py-2 mb-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
-          >
-            Generate Referral
-          </button>
-          {showLink && (
-            <div className="flex items-center mb-4">
-              <input
-                type="text"
-                value={referralLink}
-                readOnly
-                className="px-4 py-2 w-full border border-gray-300 rounded-l-md bg-gray-100 focus:outline-none"
-              />
-              <button
-                onClick={copyToClipboard}
-                className="px-4 py-2 bg-green-500 text-white font-semibold rounded-r-md hover:bg-green-600"
-              >
-                Copy
-              </button>
-            </div>
-          )}
-          <button
-            onClick={() => navigate(-1)}
-            className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-md hover:bg-gray-600"
-          >
-            Go Back
-          </button>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 
